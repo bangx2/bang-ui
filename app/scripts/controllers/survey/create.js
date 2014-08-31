398b9c8b6040b1a94e5478a304c28d79f0abf5a6
david <deggs.k@gmail.com>
2014-09-01 07:58:55 +0800

    add function of create survey

--- app/scripts/controllers/survey/create.js
+++ app/scripts/controllers/survey/create.js
@@ -2,11 +2,46 @@ define(['angular'], function (angular) {
   'use strict';
 
   angular.module('bangUiApp.controllers.SurveyCreateCtrl', [])
-    .controller('SurveyCreateCtrl', function ($scope) {
-      $scope.awesomeThings = [
-        'HTML5 Boilerplate',
-        'AngularJS',
-        'Karma'
+
+    .controller('SurveyCreateCtrl', ['$scope', '$timeout', function ($scope, $timeout) {
+
+      $scope.element_type_options = [
+        {label: '单行文本', value: 0},
+        {label: '多行文本', value: 1},
+        {label: '"是/否"选项', value: 2}
       ];
-    });
+
+      $scope.form_content = {
+        'title': '',
+        'description': '',
+        'elements': []
+      };
+
+      $scope.add_element = function () {
+        var element = {
+          'label': '',
+          'help_text': '',
+          'type': $scope.element_type_options[0].value
+        }
+        $scope.form_content.elements.push(element);
+
+        var scroll_length = $('#content>section').height();
+        $('#content').smoothScroll(scroll_length);
+      }
+
+      $scope.remove_element = function (index) {
+        $scope.form_content.splice(index, 1);
+      }
+    
+      /*
+      $timeout(function () {
+        $('#survey-preview').scrollspy({
+          animation: 'slide-left',
+          delay: 2000
+        });
+      }, 0);
+      */
+      
+
+    }]);
 });
